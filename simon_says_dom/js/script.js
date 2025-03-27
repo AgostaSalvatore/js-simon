6c468037fb562a262062561f05fb3fa734dce15a
AgostaSalvatore <agosta.boolean@gmail.com>
2025-03-27 14:08:07 +0100

    Update countdown logic and modify form and button selectors

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -3,6 +3,7 @@
 const countdown = document.getElementById('countdown');
 console.log(countdown);
 
+//instructions
 const instructions = document.getElementById('instructions');
 console.log(instructions);
 
@@ -11,22 +12,28 @@ const list = document.getElementById('number-list');
 console.log(list);
 
 //form
-const form = document.getElementById('answer-form');
+const form = document.getElementById('answers-form');
 
 //button
-const button = document.querySelector('start-button');
+const button = document.querySelector('button');
 
+//random
+const random = Math.floor(Math.random() * 50) + 1;
 
 //creo una variabile countdownValue e la inizializzo a 15
-let countdownValue = 15; 
+let countdownValue = 3; 
 
 const timer = setInterval(() => {
-    if (countdownValue > 0) {
+    if (countdownValue >= 0) {
         countdown.innerHTML  = countdownValue; // Mostra il countdown sullo schermo
         countdownValue--;
     } else {
         clearInterval(timer);
+        instructions.innerText = 'Indovina i numeri!'; // Mostra le istruzioni
+        form.classList.remove('d-none'); // Mostra il form
         countdown.innerHTML = 'Tempo scaduto'; // Mostra il messaggio di fine countdown
     }
 }, 1000);
+
+//funzione per la generazione di numeri random
 
